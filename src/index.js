682d8c0457b4cf5febbcaa4fa38bab6c786f2f30
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2020-09-28 22:49:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return Math.min.apply(0, array);
+  }
+  else return 0;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return Math.max.apply(0, array);
+  }
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return array.reduce((a, b) => a + b, 0) / array.length
+  }
+   else return 0;
 }
